912a136250d11a0ef60c01785db6f2540226f424
nathgoutam93 <nathgoutam93@gmail.com>
2022-02-12 19:10:25 +0530

    removed api keys

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyBN9GV3S4PAmNpqvznKVwAtBBqQBRvBkHc',
-  authDomain: 'linkpile-bffd7.firebaseapp.com',
-  projectId: 'linkpile-bffd7',
-  storageBucket: 'linkpile-bffd7.appspot.com',
-  messagingSenderId: '125957131390',
-  appId: '1:125957131390:web:b333e68d93687556549c2f',
-};
-export default firebaseConfig;
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import firebaseConfig from '../firebaseConfig';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
