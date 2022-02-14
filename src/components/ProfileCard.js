14b91e0e0e89c36e797cdf64a63eeba2712db75b
Mene Woron <meneworon4@gmail.com>
2022-02-14 15:48:05 +0100

    changed hardcoded svg icons to react-icons

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useFirestore } from '../context/firestoreContext';
+import React from "react";
+import { useFirestore } from "../context/firestoreContext";
+import { BsCamera } from "react-icons/bs";
 
 export default function ProfileCard({
   file,
@@ -13,16 +14,16 @@ export default function ProfileCard({
   const handleFile = (event) => {
     event.preventDefault();
 
-    dispatch({ type: 'field', field: 'file', value: event.target.files[0] });
+    dispatch({ type: "field", field: "file", value: event.target.files[0] });
   };
 
   const handleCancel = () => {
-    dispatch({ type: 'field', field: 'imgSrc', value: userData.page.imgSrc });
-    dispatch({ type: 'field', field: 'file', value: null });
+    dispatch({ type: "field", field: "imgSrc", value: userData.page.imgSrc });
+    dispatch({ type: "field", field: "file", value: null });
   };
 
   const handleRemove = () => {
-    dispatch({ type: 'field', field: 'imgSrc', value: null });
+    dispatch({ type: "field", field: "imgSrc", value: null });
   };
 
   return (
@@ -35,26 +36,9 @@ export default function ProfileCard({
             className="w-32 h-32 rounded-full object-cover border"
           />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="p-6 h-32 w-32 rounded-full border"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1}
-              d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1}
-              d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
+          <div className="p-6 h-32 w-32 rounded-full border">
+            <BsCamera className="h-full w-full" />
+          </div>
         )}
         <div className="flex-1 flex justify-center items-center space-x-1">
           {file ? (
@@ -90,8 +74,8 @@ export default function ProfileCard({
           value={profileName}
           onChange={(e) =>
             dispatch({
-              type: 'field',
-              field: 'profileName',
+              type: "field",
+              field: "profileName",
               value: e.target.value,
             })
           }
@@ -106,8 +90,8 @@ export default function ProfileCard({
           value={about}
           onChange={(e) =>
             dispatch({
-              type: 'field',
-              field: 'about',
+              type: "field",
+              field: "about",
               value: e.target.value,
             })
           }
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,37 +1,41 @@
-import { useEffect, useReducer, useState } from 'react';
-import * as ROUTES from '../constants/routes';
-import { useAuth } from '../context/authContext';
-import { useFirestore } from '../context/firestoreContext';
-import { useHeader } from '../context/headerContext';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { Droppable } from 'react-beautiful-dnd';
-import ProfileCard from '../components/ProfileCard';
-import { Link } from 'react-router-dom';
-import LinkCardEditable from '../components/LinkCardEditable';
-import Page from '../components/page';
-import Header from '../components/header';
+import { useEffect, useReducer, useState } from "react";
+import * as ROUTES from "../constants/routes";
+import { useAuth } from "../context/authContext";
+import { useFirestore } from "../context/firestoreContext";
+import { useHeader } from "../context/headerContext";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { DragDropContext } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
+import ProfileCard from "../components/ProfileCard";
+import { Link } from "react-router-dom";
+import LinkCardEditable from "../components/LinkCardEditable";
+import Page from "../components/page";
+import Header from "../components/header";
+import { IoCopyOutline } from "react-icons/io5";
+import { HiOutlineLink, HiOutlinePencil } from "react-icons/hi";
+import { BsUpload } from "react-icons/bs";
+import { AiOutlineEye } from "react-icons/ai";
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'field':
+    case "field":
       return {
         ...state,
         [action.field]: action.value,
       };
-    case 'update':
+    case "update":
       return {
         ...state,
         loading: true,
         file: null,
       };
-    case 'success':
+    case "success":
       return {
         ...state,
         loading: false,
         file: null,
       };
-    case 'error':
+    case "error":
       return {
         ...state,
         error: action.error,
@@ -43,17 +47,17 @@ function reducer(state, action) {
 
 const initialState = {
   file: null,
-  username: '',
-  imgSrc: '',
-  profileName: '',
-  about: '',
+  username: "",
+  imgSrc: "",
+  profileName: "",
+  about: "",
   links: null,
   appearance: {
-    background: '',
-    linkStyle: '',
-    font: '',
+    background: "",
+    linkStyle: "",
+    font: "",
   },
-  error: '',
+  error: "",
   loading: false,
 };
 
@@ -80,9 +84,9 @@ export default function Admin() {
 
   const handleNewLink = () => {
     dispatch({
-      type: 'field',
-      field: 'links',
-      value: [...links, { title: '', link: '', description: '', active: true }],
+      type: "field",
+      field: "links",
+      value: [...links, { title: "", link: "", description: "", active: true }],
     });
   };
 
@@ -93,14 +97,14 @@ export default function Admin() {
   const handleUpdate = async (event) => {
     event.preventDefault();
 
-    dispatch({ type: 'update' });
+    dispatch({ type: "update" });
 
     if (file) {
       const storageRef = ref(storage, `users/${userData.username}`);
       const uploadTask = uploadBytesResumable(storageRef, file);
 
       uploadTask.on(
-        'state_changed',
+        "state_changed",
         (snapshot) => {
           //uploading information
         },
@@ -119,9 +123,9 @@ export default function Admin() {
                 appearance: appearance,
               },
             });
-            dispatch({ type: 'success' });
+            dispatch({ type: "success" });
           } catch (error) {
-            dispatch({ type: 'error', error: error.message });
+            dispatch({ type: "error", error: error.message });
           }
         }
       );
@@ -136,33 +140,33 @@ export default function Admin() {
             appearance: appearance,
           },
         });
-        dispatch({ type: 'success' });
+        dispatch({ type: "success" });
       } catch (error) {
-        dispatch({ type: 'error', error: error.message });
+        dispatch({ type: "error", error: error.message });
       }
     }
   };
 
   useEffect(() => {
     if (userData) {
-      dispatch({ type: 'field', field: 'username', value: userData.username });
-      dispatch({ type: 'field', field: 'imgSrc', value: userData.page.imgSrc });
+      dispatch({ type: "field", field: "username", value: userData.username });
+      dispatch({ type: "field", field: "imgSrc", value: userData.page.imgSrc });
       dispatch({
-        type: 'field',
-        field: 'profileName',
+        type: "field",
+        field: "profileName",
         value: userData.page.profileName,
       });
-      dispatch({ type: 'field', field: 'about', value: userData.page.about });
+      dispatch({ type: "field", field: "about", value: userData.page.about });
       dispatch({
-        type: 'field',
-        field: 'appearance',
+        type: "field",
+        field: "appearance",
         value: {
           background: userData.page.appearance.background,
           linkStyle: userData.page.appearance.linkStyle,
           font: userData.page.appearance.font,
         },
       });
-      dispatch({ type: 'field', field: 'links', value: userData.page.links });
+      dispatch({ type: "field", field: "links", value: userData.page.links });
       setLoading(false);
     }
   }, [userData]);
@@ -172,7 +176,7 @@ export default function Admin() {
       const reader = new FileReader();
 
       reader.onload = function (e) {
-        dispatch({ type: 'field', field: 'imgSrc', value: e.target.result });
+        dispatch({ type: "field", field: "imgSrc", value: e.target.result });
       };
 
       reader.readAsDataURL(file);
@@ -211,28 +215,18 @@ export default function Admin() {
           className="text-sm text-gray-700 truncate underline"
         >{`https://linkpile-bffd7.web.app/${username}`}</a>
 
-        <svg
+        <IoCopyOutline
+          size={23}
+          className=" text-gray-700 cursor-pointer"
           onClick={handleShare}
-          className="w-8 h-8 text-gray-700 cursor-pointer"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1}
-            d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-          />
-        </svg>
+        />
       </div>
       <div
         className={`w-full grid ${
-          !preview && 'lg:grid-cols-2'
+          !preview && "lg:grid-cols-2"
         } bg-gray-200 font-nunito`}
       >
-        <div className={`w-full p-4  pb-96 ${preview && 'hidden'} space-y-4`}>
+        <div className={`w-full p-4  pb-96 ${preview && "hidden"} space-y-4`}>
           {error && (
             <p className="text-red-700 text-base font-semibold">{error}</p>
           )}
@@ -242,21 +236,10 @@ export default function Admin() {
               className=" text-gray-700 runcate underline"
             >{`https://linkpile-bffd7.web.app/${username}`}</a>
 
-            <svg
+            <IoCopyOutline
               onClick={handleShare}
               className="w-8 h-8 text-gray-700 cursor-pointer"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-              />
-            </svg>
+            />
           </div>
           <ProfileCard
             file={file}
@@ -273,7 +256,7 @@ export default function Admin() {
               const newLinks = [...links];
               const draggeditem = newLinks.splice(srcI, 1);
               newLinks.splice(desI, 0, ...draggeditem);
-              dispatch({ type: 'field', field: 'links', value: newLinks });
+              dispatch({ type: "field", field: "links", value: newLinks });
             }}
           >
             <Droppable droppableId="dropable-1">
@@ -305,20 +288,7 @@ export default function Admin() {
               onClick={handleNewLink}
               className="w-full px-5 py-2 flex justify-center items-center text-white bg-rose-400 rounded-xl space-x-4 hover:bg-rose-300"
             >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                />
-              </svg>
+              <HiOutlineLink size={25} />
               <span className="text-lg font-nunito">Add Link</span>
             </button>
             <button
@@ -326,29 +296,17 @@ export default function Admin() {
               onClick={handleUpdate}
               className="w-full px-5 py-2 flex justify-center items-center text-white bg-rose-400 rounded-xl space-x-4 hover:bg-rose-300"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-6 w-6 cursor-pointer ${
-                  loading && 'animate-pulse'
-                }`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                />
-              </svg>
+              <BsUpload
+                size={21}
+                className={` cursor-pointer ${loading && "animate-pulse"}`}
+              />
               <span className="text-lg font-nunito">Save</span>
             </button>
           </div>
         </div>
         <div
           className={`relative ${
-            preview ? 'flex' : 'hidden'
+            preview ? "flex" : "hidden"
           } justify-center items-center lg:flex`}
         >
           {preview ? (
@@ -356,20 +314,7 @@ export default function Admin() {
               onClick={() => setPreview(false)}
               className="fixed left-4 top-4 p-4 hidden lg:flex justify-center items-center space-x-1 bg-gray-100 rounded-3xl  cursor-pointer hover:bg-gray-50"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                />
-              </svg>
+              <HiOutlinePencil size={23} />
               <span className="text-2xl font-nunito">Editor</span>
             </div>
           ) : (
@@ -377,39 +322,20 @@ export default function Admin() {
               className="fixed top-20 p-4 hidden lg:flex items-center space-x-1 bg-gray-100 rounded-3xl cursor-pointer hover:bg-gray-50"
               onClick={() => setPreview(true)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
+              <AiOutlineEye size={25} />
               <span className="text-lg font-nunito">Preview</span>
             </div>
           )}
           <div
             className={`${
               preview
-                ? 'w-full h-screen'
-                : 'fixed top-0 w-[390px] h-[844px] scale-50'
+                ? "w-full h-screen"
+                : "fixed top-0 w-[390px] h-[844px] scale-50"
             } flex justify-center items-center bg-gray-900 rounded-3xl shadow-2xl`}
           >
             <div
               className={`${
-                preview ? 'w-full h-full' : 'w-[97%] h-[97%] rounded-3xl'
+                preview ? "w-full h-full" : "w-[97%] h-[97%] rounded-3xl"
               } flex flex-col items-center bg-gray-700 space-y-1 overflow-y-auto s_hide`}
             >
               <Page
@@ -427,87 +353,40 @@ export default function Admin() {
         {preview ? (
           <div
             onClick={() => setPreview(false)}
-            className="flex justify-center items-center space-x-1  cursor-pointer"
+            className="flex justify-center items-center space-x-1  cursor-pointer "
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-12 w-12"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-              />
-            </svg>
+            <HiOutlinePencil size={45} />
+
             <span className="text-2xl font-nunito">Editor</span>
           </div>
         ) : (
           <>
-            <div className="flex flex-col items-center">
-              <svg
+            <div className="flex flex-col items-center  ">
+              <AiOutlineEye
+                size={45}
+                className=" cursor-pointer"
                 onClick={() => setPreview(true)}
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
+              />
+
               <span className="text-lg font-nunito">Preview</span>
             </div>
             <div className="flex flex-col items-center">
-              <svg
+              <HiOutlineLink
+                size={45}
+                className="cursor-pointer"
                 onClick={handleNewLink}
-                className="h-12 w-12 cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                />
-              </svg>
+              />
               <span className="text-lg font-nunito">Add Link</span>
             </div>
             <div className="flex flex-col items-center">
-              <svg
+              <BsUpload
+                size={40}
                 disabled={loading}
                 onClick={handleUpdate}
-                xmlns="http://www.w3.org/2000/svg"
                 className={`h-12 w-12 cursor-pointer ${
-                  loading && 'animate-pulse'
+                  loading && "animate-pulse"
                 }`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                />
-              </svg>
+              />
               <span className="text-lg font-nunito">Save</span>
             </div>
           </>
