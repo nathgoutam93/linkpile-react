f5f0ccb289e865d7d227b60fccbe044db6bac187
Goutam Nath <nathgoutam93@gmail.com>
2022-02-23 00:52:10 +0530

    hot fix: toast popup position

--- src/components/commons/toast.js
+++ src/components/commons/toast.js
@@ -5,7 +5,7 @@ export default function Toast({ show, message }) {
     <div
       className={`${
         show ? "opacity-100" : "opacity-0 "
-      } fixed bottom-20 right-20 p-4 text-gray-800 font-nunito bg-gray-50 rounded-xl z-50 transition-opacity pointer-events-none`}
+      } fixed bottom-1/2 right-1/2 lg:bottom-20 lg:right-20 p-4 text-gray-800 font-nunito bg-gray-50 rounded-xl z-50 transition-opacity pointer-events-none`}
     >
       {message}
     </div>
