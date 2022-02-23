67671789b8c66384aaf50912c0fea602c9526ad1
Ahmad Tahir <30394037+A-BMT02@users.noreply.github.com>
2022-02-24 00:25:32 +0300

    Dynammic date in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function Footer() {
+  const current = new Date() ;
+  const date = `${current.getFullYear()}` ;  
+  
   return (
     <footer className="w-full h-2/6 border-t border-border-dark">
       <div className="w-full h-full p-8 bg-primary">
         <h1 className="text-5xl text-white font-nunito font-extrabold">
           Linkpile
         </h1>
-        <p className="text-white">©2022 Linkpile</p>
+        <p className="text-white">©{date} Linkpile</p>
       </div>
     </footer>
   );
